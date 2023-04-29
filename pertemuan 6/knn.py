f7fdc3ada0301904c4b55c0f86c75a0477bfc4fc
faiq raihan <faiq.fhr03@gmail.com>
2023-04-29 13:58:13 +0700

    update

---
+++ pertemuan 6/knn.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# import dataset
+dataset = pd.read_csv("Social_Network_Ads.csv")
+x = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, -1].values
+
+# splitting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.25, random_state=0
+)
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+# training k-nn model on the training set
+from sklearn.neighbors import KNeighborsClassifier
+
+classifier = KNeighborsClassifier(n_neighbors=5, metric="minkowski", p=2)
+classifier.fit(x_train, y_train)
+
+# predict the test set result
+y_pred = classifier.predict(x_test)
+
+# \making the confusuin matrix
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_test, y_pred)
+
+from matplotlib.colors import ListedColormap
+
+x_set, y_set = x_train, y_train
+x1, x2 = np.meshgrid(
+    np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
+    np.arange(start=x_set[:, 1].min() - 1, stop=x_set[:, 1].max() + 1, step=0.01),
+)
+plt.contourf(
+    x1,
+    x2,
+    classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+    alpha=0.75,
+    cmap=ListedColormap(("red", "green")),
+)
+
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(
+        x_set[y_set == j, 0],
+        x_set[y_set == j, 1],
+        c=ListedColormap(("red", "green"))(i),
+        label=j,
+    )
+plt.title("K-NN (Test Set)")
+plt.xlabel("age")
+plt.ylabel("Estimated Salary")
+plt.legend()
+plt.show()
+# khusus print sajo
+print(cm)
